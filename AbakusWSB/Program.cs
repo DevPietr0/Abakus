b7a8fe90479f04752fcfd4e4cb1d281ad8f0a949
DevPietr0 <salat899@gmail.com>
2020-07-01 12:39:08 +0200

    Initial check-in of module AbakusWSB

---
+++ AbakusWSB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbakusWSB
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
