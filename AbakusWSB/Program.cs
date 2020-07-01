5057d49fcb2f372e16045602b5202d719bd189bb
DevPietr0 <salat899@gmail.com>
2020-07-01 16:44:18 +0200

    Dodanie listy z dzialaniami w Main.

--- AbakusWSB/Program.cs
+++ AbakusWSB/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using AbakusWSB.Dzialania;
 
 namespace AbakusWSB
@@ -7,6 +8,14 @@ namespace AbakusWSB
     {
         public static void Main(string[] args)
         {
+            List<IDzialanie> dzialania = new List<IDzialanie>()
+            {
+                new Dodawanie(),
+                new Odejmowanie(),
+                new Mnozenie(),
+                new Dzielenie(),
+            };
+
             Console.WriteLine("*** Abakus ***");
             double a = 5;
             double b = 5;
