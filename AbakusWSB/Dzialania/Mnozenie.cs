8329a81b3e04ab6d997b56b69cc927127b4a986d
DevPietr0 <salat899@gmail.com>
2020-07-01 16:14:06 +0200

    Dodanie klasy Mnozenie.

---
+++ AbakusWSB/Dzialania/Mnozenie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace AbakusWSB.Dzialania
+{
+    public class Mnozenie : IDzialanie
+    {
+        public Mnozenie()
+        {
+        }
+
+        public void DrukujWynik(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Oblicz(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
