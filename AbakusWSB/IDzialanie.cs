8be2bcdf11fe3c2fef99afb1e2f17b348752b88c
DevPietr0 <salat899@gmail.com>
2020-07-01 12:44:34 +0200

    Dodanie interfejsu IDzialanie.

---
+++ AbakusWSB/IDzialanie.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AbakusWSB
+{
+    public interface IDzialanie
+    {
+        public void PobierzDane();
+        public double Oblicz();
+        public void DrukujWynik();
+    }
+}
