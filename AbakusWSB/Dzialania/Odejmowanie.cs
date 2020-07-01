edc2a781beb8f8f40b1f66986253870481448ad8
DevPietr0 <salat899@gmail.com>
2020-07-01 13:06:02 +0200

    * Dodawanie.cs: Usunięcie zbędnej metody PobierzDane()
    * Odejmowanie.cs: Dodanie klasy Odemowanie i implementacja.

--- AbakusWSB/Dzialania/Odejmowanie.cs
+++ AbakusWSB/Dzialania/Odejmowanie.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 namespace AbakusWSB.Dzialania
 {
-    public class Dodawanie : IDzialanie
+    public class Odejmowanie : IDzialanie
     {
-        public Dodawanie()
+        public Odejmowanie()
         {
         }
 
         public void DrukujWynik(double x, double y)
         {
-            Console.WriteLine($"Wynik dodawania: {Oblicz(x, y)}");
+            Console.WriteLine($"Wynik odejmowania: {Oblicz(x, y)}");
         }
 
         public double Oblicz(double x, double y)
         {
-            return x + y;
+            return x - y;
         }
-
     }
 }
