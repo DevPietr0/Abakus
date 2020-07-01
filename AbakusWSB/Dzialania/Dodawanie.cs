5765f9561018573a15bccadd92e2333a87df6d32
DevPietr0 <salat899@gmail.com>
2020-07-01 12:46:29 +0200

    Dodanie klasy Dodawanie.

---
+++ AbakusWSB/Dzialania/Dodawanie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace AbakusWSB.Dzialania
+{
+    public class Dodawanie : IDzialanie
+    {
+        public Dodawanie()
+        {
+        }
+
+        public void DrukujWynik()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Oblicz()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PobierzDane()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
